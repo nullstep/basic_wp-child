ef819b35807a6b8fc86fc0db0e9001612c062709
nullstep <me@nullstep.com>
2021-06-07 13:53:31 +0100

    container class select

--- banner.php
+++ banner.php
@@ -1,6 +1,6 @@
 <?php if (is_front_page()): ?>
 	<div id="banner-area" class="content">
-		<div class="container">
+		<div class="<?php echo getvalue('container_class'); ?>">
 			<header class="row">
 				<div class="col-sm-12 text-center">
 					<h1>banner</h1>
