ef497b00a4fa94879b8f51920f754753b7035dd1
nullstep <me@nullstep.com>
2021-06-07 13:23:13 +0100

    first update

---
+++ banner.php
@@ -0,0 +1,11 @@
+<?php if (is_front_page()): ?>
+	<div id="banner-area" class="content">
+		<div class="container">
+			<header class="row">
+				<div class="col-sm-12 text-center">
+					<h1>banner</h1>
+				</div>
+			</header>
+		</div>
+	</div>
+<?php endif; ?>
