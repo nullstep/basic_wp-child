b3e54ff62b9b7c7c0985109d540f199965b7ef31
nullstep <me@nullstep.com>
2022-06-09 12:19:44 +0100

    fix function call name change

--- banner.php
+++ banner.php
@@ -1,6 +1,6 @@
 <?php if (is_front_page()): ?>
 	<div id="banner-area" class="content">
-		<div class="<?php echo getvalue('container_class'); ?>">
+		<div class="<?php echo get_value('container_class'); ?>">
 			<header class="row">
 				<div class="col-sm-12 text-center">
 					<h1>banner</h1>
